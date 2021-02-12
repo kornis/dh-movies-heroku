6d4aaaa1a0161e9380a910df4ae36d97eea9b4c6
Federico Garcia <fedegarcia222@gmail.com>
2021-02-12 20:48:40 -0300

    cambios config sequelize

--- db/config/database.js
+++ db/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "movies_db",
-    "host": "localhost",
+  "production": {
+    "username": "kornis",
+    "password": "Digitalhouse2020.",
+    "database": "kornis_movies_db",
+    "host": "mysql-kornis.alwaysdata.net",
     "dialect": "mysql"
   },
   "test": {
@@ -13,7 +13,7 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql",
   },
-  "production": {
+  "development": {
     "username": "root",
     "password": null,
     "database": "database_production",
