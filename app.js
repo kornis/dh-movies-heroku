d666ec286051f023e8481cf40ba466eaefdde237
Federico Garcia <fedegarcia222@gmail.com>
2021-02-12 00:07:09 -0300

    puerto

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const indexRouter = require('./routes/indexRouter')
 
+let port = process.env.PORT || 3000;
+
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,4 +18,4 @@ app.use('/', indexRouter);
 
 
 
-app.listen(3000, () => console.log('Server listening to port 3000'));
+app.listen(port, () => console.log(`Server listening to port: ${port}`));
