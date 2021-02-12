9f79537ceb26d9f3a88bb358dabbb5e7dff18385
Federico Garcia <fedegarcia222@gmail.com>
2021-02-12 00:01:43 -0300

    commit

--- db/config/database.js
+++ db/config/database.js
@@ -12,7 +12,6 @@ module.exports = {
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "operatorsAliases": false
   },
   "production": {
     "username": "root",
@@ -20,6 +19,5 @@ module.exports = {
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "operatorsAliases": false
   }
 }
